99845f790dd84e6ea5ee6984442891d238a0e8f6
zored <i@ro.gl>
2020-05-28 23:01:40 +0300

    [feature/shell-completion] Add debug-http

--- src/shell-completion.ts
+++ src/shell-completion.ts
@@ -3,7 +3,8 @@ import { CompletionCommandFactory } from "./lib/shell-completion.ts";
 import { Commands } from "../mod.ts";
 import { Silent } from "./lib/command.ts";
 
-const completions = (new CompletionCommandFactory().createAll("shell-completion"))
+const completions =
+  (new CompletionCommandFactory().createAll("shell-completion"));
 await new Commands({
   ...completions,
 }, new Silent()).runAndExit();
