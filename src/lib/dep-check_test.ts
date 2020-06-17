9c75975e3a8596a1c4ae25296837db11e9c56b1a
zored <i@ro.gl>
2020-06-17 17:42:10 +0300

    [master] Fix sort-dependent test

--- src/lib/dep-check_test.ts
+++ src/lib/dep-check_test.ts
@@ -5,8 +5,12 @@ const { test } = Deno;
 test("retrieve correct deps", async () => {
   const path = "src/lib/dep-check_test/";
   const deps = await new DepFactory().allByPath(path);
-  assertEquals([
-    new Dep(path, ["a", "b", "c", "d", "e"], "a.go"),
-    new Dep(path, ["some.dep.a", "some.dep.b"], "a.kt"),
-  ], deps);
+  const byFile = (a: Dep, b: Dep) => (a.file ?? "").localeCompare(b.file ?? "");
+  assertEquals(
+    [
+      new Dep(path, ["a", "b", "c", "d", "e"], "a.go"),
+      new Dep(path, ["some.dep.a", "some.dep.b"], "a.kt"),
+    ].sort(byFile),
+    deps.sort(byFile),
+  );
 });
