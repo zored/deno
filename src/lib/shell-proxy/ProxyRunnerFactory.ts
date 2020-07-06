da1a748b41596ab2859d8caaedacd9f9c1f93f29
zored <i@ro.gl>
2020-07-06 17:49:38 +0300

    [master] Create runner with retrieve output

--- src/lib/shell-proxy/ProxyRunnerFactory.ts
+++ src/lib/shell-proxy/ProxyRunnerFactory.ts
@@ -1,9 +1,14 @@
 import { ProxyRunner } from "./ProxyRunner.ts";
 
 export class ProxyRunnerFactory {
-  fromFile = async (path: string, debug = false) =>
+  fromFile = async (
+    path: string = "shell-proxy.json",
+    debug = false,
+    retrieveOutput = false,
+  ) =>
     new ProxyRunner(
       JSON.parse(await Deno.readTextFile(path)),
       debug,
+      retrieveOutput,
     );
 }
